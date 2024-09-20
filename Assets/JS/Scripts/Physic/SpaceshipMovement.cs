ccfe6fffecc65934e45be01fdb4f5977428f6b15
Kong Ji Shou <jihsoukong@gmail.com>
2024-09-20 23:55:24 +0800

    Testing

---
+++ Assets/JS/Scripts/Physic/SpaceshipMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SpaceshipMovement : MonoBehaviour
+{
+    public float speed = 5f;  // Speed of the spaceship
+
+    private Rigidbody2D rb;  // Reference to the Rigidbody2D component
+
+    void Start()
+    {
+        // Get the Rigidbody2D component attached to the spaceship
+        rb = GetComponent<Rigidbody2D>();
+
+        // Set the spaceship to move in the right direction at the start
+        rb.velocity = transform.up * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Detect collision with other objects
+        Debug.Log("Spaceship collided with: " + collision.gameObject.name);
+
+        // Handle collision logic here, e.g., destroy the spaceship
+        // Destroy(gameObject);  // Uncomment this line if you want to destroy the spaceship on collision
+    }
+}
