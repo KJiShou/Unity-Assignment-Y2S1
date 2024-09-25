1700224e76a7d77a56dfcaa4b3c0e64122746c7c
zwzenre <zwng2005@gmail.com>
2024-09-26 01:10:54 +0800

    finish spaceship trigger victory menu but without the blurry background at the back

--- Assets/ZW/Scripts/VictoryButtons.cs
+++ Assets/ZW/Scripts/VictoryButtons.cs
@@ -5,37 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class VictoryButtons : MonoBehaviour
 {
-    IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(1);
-        SceneManager.LoadScene("Loading");
-    }
-
     public void returnHome()
     {
-        StartCoroutine(Wait());
+        SceneManager.LoadScene("StageSelect");
     }
-
-    public void replayStage()
-    {
-        StartCoroutine(Wait());
+    
+    public void toMainScene() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void nextStage()
     {
-        StartCoroutine(Wait());
-    }
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("1-3");
     }
 }
