8ee1e72ffe816ec7c824a675fe8867e359030a5a
halleyIsStressed <lee.lapyhin0127@gmail.com>
2024-09-26 19:29:50 +0800

    Stage navigation/sfx done

--- Assets/LY/Scripts/AudioManager.cs
+++ Assets/LY/Scripts/AudioManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [Header("---------- Audio Source ----------")]
-    [SerializeField] AudioSource musicSource;
+    public AudioSource musicSource;
     [SerializeField] AudioSource SFXSource;
 
     [Header("---------- Audio Clip ----------")]
