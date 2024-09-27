908839868de980c0800c73d650f857b0274939bc
zwzenre <zwng2005@gmail.com>
2024-09-27 21:06:21 +0800

    done saved prefab and spawn it

--- Assets/JS/Scripts/ChangeSpaceShip.cs
+++ Assets/JS/Scripts/ChangeSpaceShip.cs
@@ -1,68 +1,59 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class ChangeSpaceShip : MonoBehaviour
 {
     [SerializeField]
-    Sprite[] imageList;
+    Sprite[] imageList;  // List of spaceship skins
     public Image image;
-    private int backup;
-    private float backupR = 1;
-    private float backupG = 1;
-    private float backupB = 1;
+
+    private int backupIndex;
 
     AudioManager audioManager;
-    void Start() {
 
+    void Start()
+    {
         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
-    
-        backup = GameManager.Instance.spaceshipIndex;
-        backupR = GameManager.Instance.spaceshipColor.r;
-        backupG = GameManager.Instance.spaceshipColor.g;
-        backupB = GameManager.Instance.spaceshipColor.b;
+        
+        // Backup the current spaceship index
+        backupIndex = GameManager.Instance.spaceshipIndex;
+
+        // Set the initial spaceship skin
         image.sprite = imageList[GameManager.Instance.spaceshipIndex];
-        image.color = GameManager.Instance.spaceshipColor;
+        image.color = GameManager.Instance.spaceshipColor;  // Set the current color from RGBColor
     }
-    public void leftImage() {
+
+    public void leftImage()
+    {
         audioManager.PlaySFX(audioManager.changeSkinSwipe);
+        GameManager.Instance.spaceshipIndex = (GameManager.Instance.spaceshipIndex > 0)
+            ? GameManager.Instance.spaceshipIndex - 1
+            : imageList.Length - 1;
 
-        if (GameManager.Instance.spaceshipIndex>0) {
-            GameManager.Instance.spaceshipIndex--;
-            image.sprite = imageList[GameManager.Instance.spaceshipIndex];
-            image.color = GameManager.Instance.spaceshipColor;
-        }else {
-            GameManager.Instance.spaceshipIndex = imageList.Length-1;
-            image.sprite = imageList[GameManager.Instance.spaceshipIndex];
-            image.color = GameManager.Instance.spaceshipColor;
-        }
+        image.sprite = imageList[GameManager.Instance.spaceshipIndex];
     }
 
-    public void rightImage() {
+    public void rightImage()
+    {
         audioManager.PlaySFX(audioManager.changeSkinSwipe);
+        GameManager.Instance.spaceshipIndex = (GameManager.Instance.spaceshipIndex < imageList.Length - 1)
+            ? GameManager.Instance.spaceshipIndex + 1
+            : 0;
 
-        if (GameManager.Instance.spaceshipIndex<imageList.Length-1) {
-            GameManager.Instance.spaceshipIndex++;
-            image.sprite = imageList[GameManager.Instance.spaceshipIndex];
-            image.color = GameManager.Instance.spaceshipColor;
-        }else{
-            GameManager.Instance.spaceshipIndex = 0;
-            image.sprite = imageList[GameManager.Instance.spaceshipIndex];
-            image.color = GameManager.Instance.spaceshipColor;
-        }
+        image.sprite = imageList[GameManager.Instance.spaceshipIndex];
     }
 
-    public void cancel() {
-
+    public void cancel()
+    {
         audioManager.PlaySFX(audioManager.menuClickOut);
-        GameManager.Instance.spaceshipIndex = backup;
-        GameManager.Instance.spaceshipColor = new Color(backupR, backupG, backupB);
+        GameManager.Instance.spaceshipIndex = backupIndex;
         SceneManager.LoadScene("Main");
     }
 
-    public void submit(){
+    public void submit()
+    {
         audioManager.PlaySFX(audioManager.menuClickIn);
         SceneManager.LoadScene("Main");
     }
